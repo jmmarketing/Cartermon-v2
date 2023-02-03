8976efcf89ca56bde3d91a5db96e2a0d53c77ec2
jmmarketing <jeff@jeffreymclean.com>
2023-02-03 14:35:20 -0800

    Defined html element variables in JS file

---
+++ app.js
@@ -0,0 +1,4 @@
+const container = document.querySelector('.search-container');
+const searchElement = document.getElementById('search');
+const searchInput = document.querySelector('input[name="search-bar"]');
+const searchButton = document.querySelector('#submit');
