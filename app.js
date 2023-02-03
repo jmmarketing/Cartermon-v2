6a5d8102c5d4109cfcd57f1775a526de2716ab49
jmmarketing <jeff@jeffreymclean.com>
2023-02-03 15:07:31 -0800

    Added testing function for search form submit

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const container = document.querySelector('.search-container');
 const searchElement = document.getElementById('search');
 const searchInput = document.querySelector('input[name="search-bar"]');
-const searchButton = document.querySelector('#submit');
+const submit = document.getElementById('search-bar');
+
+function searchPokemon(e) {
+    e.preventDefault();
+    
+    let pokeSearchValue = e.srcElement[0].value;
+    console.log(pokeSearchValue);
+
+
+    searchInput.value = '';
+}
+
+submit.addEventListener('submit', searchPokemon);
