4033c530a0c4095fedc51969263b8ff45b612b88
jmmarketing <jeff@jeffreymclean.com>
2023-02-03 15:58:17 -0800

    Build out search loader logic. Updated element attributes, css, and expanded basic function tests

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const container = document.querySelector('.search-container');
 const searchElement = document.getElementById('search');
 const searchInput = document.querySelector('input[name="search-bar"]');
 const submit = document.getElementById('search-bar');
+const loader = document.querySelector('.loader');
 
 function searchPokemon(e) {
     e.preventDefault();
+
+    searchElement.hidden = true;
+    loader.hidden = false;
+    searchInput.value = '';
     
     let pokeSearchValue = e.srcElement[0].value;
     console.log(pokeSearchValue);
 
-
-    searchInput.value = '';
+    
 }
 
 submit.addEventListener('submit', searchPokemon);
