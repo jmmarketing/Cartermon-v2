5d3ba4c58bd1edb244024ae8585be30d2c02f107
jmmarketing <jeff@jeffreymclean.com>
2023-02-06 13:48:52 -0800

    Started testing PokeAPI end points

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const searchElement = document.getElementById('search');
 const searchInput = document.querySelector('input[name="search-bar"]');
 const submit = document.getElementById('search-bar');
 const loader = document.querySelector('.loader');
+let names = [];
 
 function searchPokemon(e) {
     e.preventDefault();
@@ -18,3 +19,21 @@ function searchPokemon(e) {
 }
 
 submit.addEventListener('submit', searchPokemon);
+
+async function pokeNames() {
+
+    try {
+    const response = await fetch ('https://pokeapi.co/api/v2/pokemon?limit=151');
+
+    if (response.ok) {
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+        }
+    // throw new Error('Request Failed!')
+
+    } catch(error){
+        console.log(error);
+    }
+}
+
+window.onload = pokeNames;
