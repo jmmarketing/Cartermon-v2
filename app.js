32932d41a88a38b5369254c39cdb7bf179eb6782
jmmarketing <jeff@jeffreymclean.com>
2023-02-07 14:14:19 -0800

    Added needed info from response into pokemon object. Finished function create pokemon result card from object

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 // ######## GLOBAL VARIABLES ############
+//------- Selectors ----------
 const container = document.querySelector('.search-container');
 const searchElement = document.getElementById('search');
 const searchInput = document.querySelector('input[name="search-bar"]');
 const submit = document.getElementById('search-bar');
 const loader = document.querySelector('.loader');
+const results = document.querySelector('.results');
+
+//------- Arrays & Objects ----------
 let names = [];
+let pokemon = {
+    name: '',
+    hp: '',
+    img: '',
+    attack: '',
+    speed: '',
+    defense: '',
+    special_attack: '',
+    special_defense: ''
+};
 
 //########## Grab & Store Pokemon Names for Autocomplete ##########
 async function loadPokeNames() {
@@ -39,9 +53,19 @@ async function searchPokemon(e) {
         const pokeResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeSearchValue}`);
         if (pokeResponse.ok) {
             const pokeJSON = await pokeResponse.json();
-            console.log(pokeJSON);
+            pokemon.name = pokeJSON["name"];
+            pokemon.img = pokeJSON["sprites"]["other"]["official-artwork"]["front_default"];
+            pokemon.hp = pokeJSON["stats"][0]["base_stat"];
+            pokemon.attack = pokeJSON["stats"][1]["base_stat"];
+            pokemon.speed = pokeJSON["stats"][5]["base_stat"];
+            pokemon.defense = pokeJSON["stats"][2]["base_stat"];
+            pokemon.special_attack = pokeJSON["stats"][3]["base_stat"];
+            pokemon.special_defense = pokeJSON["stats"][4]["base_stat"];
+
+            console.log(pokemon);
+            createPokeCard(pokemon);
         } else {
-            throw new Error('Something Went Wrong!');
+            throw new Error("Something Went Wrong.");
         }
     } catch (error) {
         console.log(error);
@@ -50,7 +74,32 @@ async function searchPokemon(e) {
 }
 
 // ####### Generates the Pokemon Card #########
-function createPokeCard() {
+function createPokeCard(object) {
+    const pokeName = document.querySelector('#poke-name p');
+    const pokeHP = document.querySelector('#hp');
+    const pokeImg = document.querySelector('#poke-image img');
+    const pokeAttack = document.querySelector('#attack .num');
+    const pokeSpeed= document.querySelector('#speed .num');
+    const pokeDefense = document.querySelector('#defense .num');
+    const pokeSpecialA = document.querySelector('#special-attack .num');
+    const pokeSpecialD = document.querySelector('#special-defense .num');
+
+    console.log(pokeAttack)
+
+    pokeName.textContent = object.name;
+    pokeHP.textContent = `${object.hp} HP`;
+    pokeImg.src = object.img;
+    pokeAttack.innerText = object.attack;
+    pokeDefense.textContent = object.defense;
+    pokeSpeed.textContent = object.speed;
+    pokeSpecialA.textContent = object.special_attack;
+    pokeSpecialD.textContent = object.special_defense;
+
+    setTimeout(() => {
+        loader.hidden = true;
+        results.hidden = false;
+    }, 3000)
+    
 
 }
 
