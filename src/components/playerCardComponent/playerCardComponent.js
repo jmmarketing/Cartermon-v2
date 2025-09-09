16d3912f8e84a91d21e83949158e364cf92ff17e
jmmarketing <jeff@jeffreymclean.com>
2025-09-09 12:13:32 -0700

    DONE: Restrcuture of playerCardComponent to include (mini & main config option). Started build out of gameplay-card elements structure ands styling.

--- src/components/playerCardComponent/playerCardComponent.js
+++ src/components/playerCardComponent/playerCardComponent.js
@@ -1,28 +1,59 @@
 import { icons, avatars } from "../../../assets/game-assets.js";
 
-export const playerCardComponent = (playerObj) => {
-  return `
-    <div class="player-card" data-id="${playerObj.id}">
+export const playerCardComponent = (
+  playerData,
+  config = { cardSize: "mini" }
+) => {
+  const { cardSize } = config;
+
+  if (cardSize !== "main") cardSize = "mini";
+
+  const miniHTML = `
         <div class="player-card__avatar-box">
               <img
                 class="player-card__img"
-                src="${avatars[playerObj.avatar]}"
-                alt="${playerObj.name} Player"
+                src="${avatars[playerData.avatar]}"
+                alt="${playerData.name} Player"
               />
         </div>
-        <p class="user-name">${playerObj.name}</p>
-        <div class="player-card__pokeball-info">
+        <p class="user-name">${playerData.name}</p>
+        <div class="player-card__info-title">
               <p class="play-information">
                 <span
                   ><img
                     src="${icons.pixelPokeball}"
                     alt="Pokeball"
-                    class="player-card__pokeball-info--img"
+                    class="player-card__info-title--img"
                   />
                 </span>
-                X ${playerObj.pokeballs}
+                X ${playerData.pokeballs}
               </p>
         </div>
-    </div>
+    
     `;
+
+  const mainHTML = `
+    <div class="player-card__info-title">
+      <p class="user-title">Pokemon Caught</p>
+    </div>
+    <p class="rowdie-stat stat-blue">${playerData.caught.length}</p>
+
+    <div class="player-card__info-title">
+        <p class="user-title">Math Questions</p>
+    </div>
+    <p class="rowdie-stat stat-purple">${playerData.answers}</p>
+    
+    `;
+
+  const template = `
+    <div class="player-card player-card--${cardSize}" data-id="${
+    playerData.id
+  }">
+    ${miniHTML}
+    ${cardSize === "main" ? mainHTML : ""}
+    </div>
+    
+    `;
+
+  return template;
 };
