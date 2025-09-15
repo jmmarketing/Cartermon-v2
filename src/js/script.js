80a192e18fb0f21ea02143b16dc5700a06c94488
jmmarketing <jeff@jeffreymclean.com>
2025-09-15 11:39:21 -0700

    IN PROGRESS: Psuedo logic for learning View & MathGen function.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,41 @@
-import { avatars } from "../../assets/game-assets.js";
+const questionsContainer = document.querySelector(
+  ".learn__math-container--questions"
+);
+const messageBar = document.querySelector(".learn__title p");
+const submitBtn = document.querySelector(".learn__check-button");
+const allQuestionContainers = document.querySelectorAll(".math-question");
+const mathForm = document.querySelector(".learn__math-quiz");
 
-console.log(Object.entries(avatars));
+/*
+-Generate Math Questions = 
+{
+q1:{
+    operand: '+ -',
+    top: number,
+    bottom: number,
+    answer: number,
+    choices: [answer, wrong, wrong, wrong]
+},
+q2:{},
+q3:{}
+}
+
+
+Creates Math Question Cards-
+- Loops through questions Object. Creates math card for each question.
+- Randomize answer display. 
+- Add appropriate attributes to input (name, id, value)
+
+On Submit-
+- Loop through selected answers. 
+- If correct add (correct class & disable fieldset?), if wrong add (wrong class) 
+- Record/store correct number of questions. (view State?)
+- Update Message element to reflect number correct. 
+
+- If all 3 correct, show pokeball screen & update player stat of pokeball & questions.
+- Disable Check answers button. 
+- Play-again button regenerates math questions. 
+
+- If any incorrect. 
+
+*/
