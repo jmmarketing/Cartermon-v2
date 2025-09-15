662c4b1b78bb3573f84679440e148ce2f8d7aac0
jmmarketing <jeff@jeffreymclean.com>
2025-09-15 16:10:06 -0700

    START: Buildout of check-answers event listener. FormData doesnt make sense as it doesnt provide necessary input details for UX steps. Working on grabbing all inputs and running a loop to check state of :checked. Will then have to use mathObject to check answers against and add classes/disabled to fieldset for specific conditions.

--- src/js/script.js
+++ src/js/script.js
@@ -6,10 +6,33 @@ const submitBtn = document.querySelector(".learn__button--check");
 const playAgainBtn = document.querySelector(".learn__button--play-again");
 const allQuestionContainers = document.querySelectorAll(".math-question");
 const mathForm = document.querySelector(".learn__math-quiz");
+const allRadioAnswers = document.querySelectorAll("input");
 
 const successContainer = document.querySelector(".learn__success-container");
 
-let mathQuestionObj = {};
+let mathQuestionObj = {
+  q1: {
+    operand: "+",
+    top: 117,
+    bottom: 117,
+    answer: 234,
+    choices: [27, 7, 25, 24],
+  },
+  q2: {
+    operand: "+",
+    top: 17,
+    bottom: 10,
+    answer: 27,
+    choices: [7, 27, 11, 22],
+  },
+  q3: {
+    operand: "-",
+    top: 117,
+    bottom: 100,
+    answer: 17,
+    choices: [17, 183, 447, 517],
+  },
+};
 
 /*
 -Generate Math Questions = 
@@ -44,3 +67,27 @@ On Submit-
 - If any incorrect. 
 
 */
+
+submitBtn.addEventListener("click", function () {
+  //   const answerData = new FormData(mathForm);
+  //   console.log("Getting Answers");
+  //   console.log(answerData);
+
+  //   for (const value of answerData.values()) {
+  //     console.log(value);
+  //   }
+
+  allRadioAnswers.forEach((input) => {
+    if (input.checked) {
+      console.log("Input Is:");
+      console.dir(input);
+
+      console.log("Is selected answer for:" + input.name);
+      console.log("Has ID of: " + input.id);
+      console.log("Value Is: " + input.value);
+
+      console.log("Is Selected: " + input.checked);
+      console.log("#--#---#---#----#----#---#");
+    }
+  });
+});
