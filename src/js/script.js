8dd253b803ca586b13bdd0e3ce21d2b6f826995c
jmmarketing <jeff@jeffreymclean.com>
2025-09-23 15:44:50 -0700

    Started HTML/SCSS build out of explore page.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,3 @@
-import { learnTemplate } from "./views/learnView/learnTemplate.js";
 import * as model from "./model.js";
-import { mathCardComponent } from "../components/mathCardComponent/mathCardComponent.js";
+import { playerCardComponent } from "../components/playerCardComponent/playerCardComponent.js";
+import { infoBarComponent } from "../components/infoBarComponent/infoBarComponent.js";
