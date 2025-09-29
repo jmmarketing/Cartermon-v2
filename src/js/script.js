15a23890d1a06d0fa356dcea80a1f69f8021a927
jmmarketing <jeff@jeffreymclean.com>
2025-09-29 15:39:08 -0700

    Basic empty state of pokemon-details built

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,1 @@
 import * as model from "./model.js";
-import { playerCardComponent } from "../components/playerCardComponent/playerCardComponent.js";
-import { infoBarComponent } from "../components/infoBarComponent/infoBarComponent.js";
